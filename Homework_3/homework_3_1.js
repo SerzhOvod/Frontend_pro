692687fd3dc5dd538d264fa0166879af49a4973e
SerzOvod <sergey.ovodenko@gmail.com>
2026-04-30 01:34:01 +0300

    first commit

---
+++ Homework_3/homework_3_1.js
@@ -0,0 +1,9 @@
+let surname;
+console.log(typeof 3);
+console.log(typeof 'Hello, world!');
+console.log(typeof true);
+console.log(typeof surname);
+console.log(typeof { user: 'name', email: 'name.surname@gmail.com' });
+console.log(typeof 1234567890n);
+console.log(typeof Symbol());
+console.log(typeof function () {});
---
+++ Homework_3/homework_3_2.js
@@ -0,0 +1,4 @@
+let firstLine = prompt('Enter your name:');
+let secondLine = prompt('Enter your surname:');
+let thirdLine = prompt('Enter your e-mail:');
+console.log(`${secondLine}, ${firstLine}, ${thirdLine}`);
