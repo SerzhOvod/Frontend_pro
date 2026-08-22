6d718fa744e04651c1f2fbe5e917f76f648565fa
SerzOvod <sergey.ovodenko@gmail.com>
2026-08-22 18:32:05 +0300

    deploy swapi redux app to github pages

--- Homework_30/swapi-redux-app/vite.config.js
+++ Homework_30/swapi-redux-app/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/Frontend_pro/Homework_30/swapi-redux-app/',
